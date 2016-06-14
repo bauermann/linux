95d59cfd61b18a0899a51bd3e54aee49b50cc83d
Thiago Jung Bauermann <bauerman@linux.vnet.ibm.com>
2016-06-13 22:38:43 -0300

    powerpc: kexec_file: Add buffer hand-over support for the next kernel
    The buffer hand-over mechanism allows the currently running kernel to pass
    data to kernel that will be kexec'd via a kexec segment. The second kernel
    can check whether the previous kernel sent data and retrieve it.
    
    This is the architecture-specific part.
    
    Signed-off-by: Thiago Jung Bauermann <bauerman@linux.vnet.ibm.com>

--- arch/powerpc/kernel/kexec_elf_64.c
+++ arch/powerpc/kernel/kexec_elf_64.c
@@ -219,7 +219,7 @@ void *elf64_load(struct kimage *image, char *kernel_buf,
 		}
 	}
 
-	ret = setup_new_fdt(fdt, initrd_load_addr, initrd_len, cmdline);
+	ret = setup_new_fdt(image, fdt, initrd_load_addr, initrd_len, cmdline);
 	if (ret)
 		goto out;
 
